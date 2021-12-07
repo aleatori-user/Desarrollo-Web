deea93cff3909312d1e74a3c2cbdf1e484e78f05
Walther Mera <waltf15@gmail.com>
2021-12-07 13:38:34 -0500

    FrontEnd v9.6

--- src/app/componentes/administrador/administrador.component.ts
+++ src/app/componentes/administrador/administrador.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-administrador',
-    templateUrl: './administrador.component.html',
-    styleUrls: ['./administrador.component.css']
+  selector: 'app-administrador',
+  templateUrl: './administrador.component.html',
+  styleUrls: ['./administrador.component.css'],
 })
-
 export class AdministradorComponent implements OnInit {
+  constructor(private router: Router) {}
 
-    constructor(){
+  ngOnInit(): void {}
 
-    }
-
-    ngOnInit(): void{
-
-    }
- 
+  adminUsuario() {
+    this.router.navigate(['administrador-usuario']);
+  }
+  adminVeterinario() {
+    this.router.navigate(['administrador-veterinario']);
+  }
+  registroVeterinario() {
+    this.router.navigate(['administrador-registro-veterinario']);
+  }
 }
